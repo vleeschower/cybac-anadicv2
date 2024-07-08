d8e409741a044014f1e26ae106995c6c773431ae
vleeschower <144490940+vleeschower@users.noreply.github.com>
2024-07-08 11:24:53 -0600

    pagina contacto

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\RegistroController;
-use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WebController;
+use App\Http\Controllers\MailController;
+use App\Http\Controllers\RegistroController;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,9 +29,13 @@ Route::get('/noticias',[WebController::class,'noticias'])->name('noticias');
 
 Route::get('/afiliados',[WebController::class,'afiliados'])->name('afiliados');
 
-Route::get('/contacto',[WebController::class,'contacto'])->name('contacto');
+//Route::get('/contacto',[WebController::class,'contacto'])->name('contacto');
 
 Route::get('/registro',[WebController::class,'registro'])->name('registro');
 Route::post('/registro', [WebController::class, 'registro2'])->name('registro2');
 
 Route::get('/administrador',[WebController::class,'administrador'])->name('administrador');
+
+//rutas para el envio de emailsPHPMailer
+Route::get('/contacto',[MailController::class, 'emailsPHPMiller'])->name('contacto');
+Route::post('/contacto',[MailController::class, 'storeEmails'])->name('contacto');
