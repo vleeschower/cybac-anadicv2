78c28c796955dd5741e5f07903d84f538a547915
vleeschower <144490940+vleeschower@users.noreply.github.com>
2024-07-09 11:50:50 -0600

    archivo web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,10 @@
 <?php
 
-use App\Http\Controllers\Administrador\NoticiasController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\MailController;
-use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-
-
-
-
+use App\Http\Controllers\WebController;
+use App\Http\Controllers\MailController;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,17 +24,6 @@ Route::get('/', [App\Http\Controllers\WebController::class, 'index'])->name('ind
 
 Route::get('/quienes_somos',[WebController::class,'quienes_somos'])->name('quienes_somos');
 
-// Route::get('/noticias',[WebController::class,'noticias'])->name('noticias');
-
-Route::get('/afiliados',[WebController::class,'afiliados'])->name('afiliados');
-
-//solo puede haber un metodo get para controlar la ruta de contacto
-// Route::get('/contacto',[WebController::class,'contacto'])->name('contacto');
-
-//rutas para el envio de emailsPHPMailer
-Route::get('/contacto',[MailController::class, 'emailsPHPMiller'])->name('contacto');
-Route::post('/contacto',[MailController::class, 'storeEmails'])->name('contacto');
-
 Route::get('/noticias',[WebController::class,'noticias'])->name('noticias');
 
 Route::get('/afiliados',[WebController::class,'afiliados'])->name('afiliados');
@@ -50,17 +32,6 @@ Route::get('/registro',[WebController::class,'registro'])->name('registro');
 Route::post('/registro', [WebController::class, 'registro2'])->name('registro2');
 
 Route::get('/administrador',[WebController::class,'administrador'])->name('administrador');
-
-Route::get('/prueba',[WebController::class,'prueba'])->name('prueba');
-
-//visualizacion de texto-noticia
-Route::resource('noticias',NoticiasController::class);
-
-//ruta para el login 
-Route::get('/register',[RegisterController::class,'create'])->name('login');
-
-//ruta para el apartadp de registro
-Route::get('/login',[LoginController::class,'create'])->name('login');
 
 //rutas para el envio de emailsPHPMailer
 Route::get('/contacto',[MailController::class, 'emailsPHPMiller'])->name('contacto');
